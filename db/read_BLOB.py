9eb077b612562d6e54a8f410689ed4275099e1be
lolo115 <leturgezl@gmail.com>
2019-01-16 21:30:35 +0100

    commit

--- db/read_BLOB.py
+++ db/read_BLOB.py
@@ -1,6 +1,9 @@
 import os
 import cx_Oracle
 import sys
+import numpy as np
+from PIL import Image
+from io import BytesIO
 
 if sys.platform=='darwin':
     root_dir = '/Users/leturgezl/PycharmProjects/work'
@@ -19,8 +22,22 @@ if __name__ == '__main__':
         c = cx_Oracle.Connection (user="laurent", password="laurent", dsn="192.168.99.3:1521/orcl")
 
         cur = c.cursor()
-        cur.execute('select status,b from bin_file where rownum=1')
+        cur.execute('select b,status from bin_file where rownum=1')
         r=cur.fetchall()
+
+        c1 = [row[0] for row in r][0]
+        print("type(c1)=", type(c1))
+        c2 = [row[1] for row in r][0]
+        print("c2=", c2)
+
+
+        s=c1.read()
+        im=Image.open(BytesIO(s))
+        imgsize=32,32
+        im.thumbnail(imgsize)
+        A=np.array(im,dtype=np.float32)
+        print("A=",A)
+
         cur.close()
     except cx_Oracle.DatabaseError as ex:
         err, =ex.args
@@ -29,4 +46,5 @@ if __name__ == '__main__':
         c.close()
     c.close()
 
-    print("r",r)
+
+
